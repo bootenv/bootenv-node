e5047a4bf7380f589d14ee328d7f30e90d581a1a
acactown <acactown@gmail.com>
2015-06-28 13:38:59 -0300

    Add first version
    Signed-off-by: acactown <acactown@gmail.com>

--- environment.js
+++ environment.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var debug = require('debug')('environment');
-var Optional = require('optional');
+var Optional = require('guava-optional');
+
+var DEFAULT_NAME = 'development';
 
 /**
  * @private
@@ -12,8 +14,25 @@ function safeSupports(value) {
   return (value.trim().toLowerCase() === 'true');
 }
 
+/**
+ * @private
+ * @returns {Optional} Returns An object containing the user environment
+ */
+function getEnvironment() {
+  if (process && typeof process !== 'undefined') {
+    return Optional.fromNullable(process.env);
+  }
+
+  return Optional.absent();
+}
+
 var Environment = function() {
-  this.name = (process.env.NODE_ENV || 'development');
+  var environment = getEnvironment();
+  if (environment.isPresent()) {
+    this.name = (environment.get().NODE_ENV || DEFAULT_NAME);
+  } else {
+    this.name = DEFAULT_NAME;
+  }
 };
 
 /**
@@ -28,6 +47,62 @@ Environment.prototype.getName = function() {
 };
 
 /**
+ * Get a `Optional` property value according to Environment
+ *
+ * @param {String} key The property name
+ * @returns {Optional} Returns the `Optional` property value according to Environment
+ */
+Environment.prototype.getOptionalProperty = function(key) {
+  debug('Getting [%s] property from Environment...', key);
+
+  var environment = getEnvironment();
+  if (environment.isPresent()) {
+    try {
+      var properties = environment.get();
+      if (properties.hasOwnProperty(key)) {
+        var value = properties[key];
+        if (value && typeof value === 'string' && value !== 'undefined' && value !== 'null') {
+          return Optional.of(value);
+        }
+      }
+    } catch (error) {
+      debug('Error getting Environment property for key [%s]!', key, error);
+    }
+  }
+
+  return Optional.absent();
+};
+
+/**
+ * Get the property value according to Environment,
+ * if it's not present will return the `defaultValue`
+ *
+ * @param {String} key The property name
+ * @param {String} defaultValue The  default value
+ * @returns {String} Returns the property value according to Environment
+ */
+Environment.prototype.getPropertyOr = function(key, defaultValue) {
+  var property = this.getOptionalProperty(key);
+  if (property.isPresent()) {
+    return property.get();
+  }
+
+  debug('Environment has no the [%s] property, returning default value [%s]', key, defaultValue);
+  return defaultValue;
+};
+
+/**
+ * Get the property value according to Environment,
+ * use only if you are 100% sure that the environment has a `key`
+ *
+ * @param {String} key The property name
+ * @returns {String} Returns the property value according to Environment, otherwise returns `null`
+ */
+Environment.prototype.getProperty = function(key) {
+  return this.getPropertyOr(key, null);
+};
+
+/**
  * Checks if a `key` is present
  *
  * @param {String} key The value to check.
@@ -36,7 +111,9 @@ Environment.prototype.getName = function() {
 Environment.prototype.hasProperty = function(key) {
   debug('Checking is Environment has the [%s] property...', key);
 
-  return process.env[key];
+  var property = this.getOptionalProperty(key);
+
+  return property.isPresent();
 };
 
 /**
@@ -58,7 +135,7 @@ Environment.prototype.supports = function(feature) {
  * if it's not present will return the `defaultValue`
  *
  * @param {String} feature The `feature` name
- * @param  {boolean} defaultValue The  default value
+ * @param  {boolean} defaultValue The `default value`
  * @returns {boolean} Returns `true` only if the environment has a property and the value is `true`,
  * otherwise: returns the `defaultValue`
  */
@@ -74,52 +151,75 @@ Environment.prototype.supportsOr = function(feature, defaultValue) {
 };
 
 /**
- * Get a `Optional` property value according to Environment
+ * Get a `Optional` property number value according to Environment
  *
  * @param {String} key The property name
- * @returns {Optional} The `Optional` property value according to Environment
+ * @returns {Optional} Returns the `Optional` property number value according to Environment
  */
-Environment.prototype.getOptionalProperty = function(key) {
-  debug('Checking is Environment has the [%s] property...', key);
+Environment.prototype.getOptionalNumber = function(key) {
+  var property = this.getOptionalProperty(key);
+  if (property.isPresent()) {
+    var number = property.get();
 
-  try {
-    var value = process.env[key];
-    if (value && value !== undefined && typeof value === 'string') {
-      return Optional.fromNullable(value);
+    if (typeof number === 'number') {
+      return Optional.of(number);
     }
-  } catch (error) {
-    debug('Error getting Environment property for key [%s]!', key, error);
+
+    try {
+      return Optional.fromNullable(Number(number));
+    } catch (error) {
+      debug('Error getting number from string [%s]!', number, error);
+    }
   }
 
   return Optional.absent();
 };
 
 /**
- * Get the property value according to Environment,
+ * Get the property number value according to Environment,
  * if it's not present will return the `defaultValue`
  *
  * @param {String} key The property name
- * @param {String} defaultValue The  default value
- * @returns {String} the property value according to Environment
+ * @param {Number} defaultValue The  default value
+ * @returns {Number} Returns the property number value according to Environment
  */
-Environment.prototype.getPropertyOr = function(key, defaultValue) {
-  var property = this.getOptionalProperty(key);
-  if (property.isPresent()) {
-    return property.get();
+Environment.prototype.getNumberOr = function(key, defaultValue) {
+  var number = this.getOptionalNumber(key);
+  if (number.isPresent()) {
+    return number.get();
   }
 
+  debug('Environment has no the [%s] property, returning default value [%s]', key, defaultValue);
   return defaultValue;
 };
 
 /**
- * Get the property value according to Environment,
- * use only if you are 100% sure that the environment has a `key`
+ * Get the property number value according to Environment,
+ * use only if you are 100% sure that the environment has a key,
  *
  * @param {String} key The property name
- * @returns {String} the property value according to Environment, otherwise returns `null`
+ * @returns {Number} Returns the property number value according to Environment, otherwise returns `null`
  */
-Environment.prototype.getProperty = function(key) {
-  return this.getPropertyOr(key, null);
+Environment.prototype.getNumber = function(key) {
+  return this.getNumberOr(key, null);
+};
+
+/**
+ * @returns {Array} Returns an Array`with all Environment keys
+ */
+Environment.prototype.getKeys = function() {
+  var keys = [];
+  var environment = getEnvironment();
+  if (environment.isPresent()) {
+    var properties = environment.get();
+    for (var key in properties) {
+      if (properties.hasOwnProperty(key)) {
+        keys.push(key);
+      }
+    }
+  }
+
+  return keys;
 };
 
 module.exports = new Environment();
--- test.js
+++ test.js
@@ -1,1 +1,178 @@
 'use strict';
+
+var test = require('tap').test;
+var environment = require('./environment');
+
+var KEY1 = 'KEY1';
+var KEY2 = 'KEY2';
+var KEY3 = 'KEY3';
+var KEY4 = 'KEY4';
+var KEY5 = 'KEY5';
+var KEY6 = 'KEY6';
+
+var VALUE1 = ' TrUe';
+var VALUE2 = 'fAlSe ';
+var DEFAULT_VALUE = 'DEFAULT';
+var DEFAULT_NUMBER = 10;
+
+function loadProperties() {
+  process.env[KEY1] = VALUE1;
+  process.env[KEY2] = VALUE2;
+  process.env[KEY3] = null;
+  process.env[KEY4] = '';
+}
+
+function loadNumberProperties() {
+  process.env[KEY1] = '1000000';
+  process.env[KEY2] = '2.5';
+  process.env[KEY3] = '-3';
+  process.env[KEY4] = null;
+  process.env[KEY5] = 'abc123';
+  process.env[KEY6] = '';
+}
+
+test('>bootenv environment', function(tap) {
+
+  tap.test('getName', function(t) {
+    t.equal('development', environment.getName());
+
+    t.end();
+  });
+
+  tap.test('hasProperty', function(t) {
+    loadProperties();
+
+    t.ok(environment.hasProperty(KEY1));
+    t.ok(environment.hasProperty(KEY2));
+    t.notOk(environment.hasProperty(KEY3));
+    t.notOk(environment.hasProperty(KEY4));
+
+    t.end();
+  });
+
+  tap.test('supports', function(t) {
+    loadProperties();
+
+    t.ok(environment.supports(KEY1));
+    t.notOk(environment.supports(KEY2));
+    t.notOk(environment.supports(KEY3));
+    t.notOk(environment.supports(KEY4));
+
+    t.end();
+  });
+
+  tap.test('supportsOr', function(t) {
+    loadProperties();
+
+    t.ok(environment.supportsOr(KEY1, false));
+    t.notOk(environment.supportsOr(KEY2, true));
+    t.ok(environment.supportsOr(KEY3, true));
+    t.ok(environment.supportsOr(KEY4, true));
+
+    t.end();
+  });
+
+  tap.test('getOptionalProperty', function(t) {
+    loadProperties();
+
+    var property = environment.getOptionalProperty(KEY1);
+    t.ok(property.isPresent());
+    t.equal(VALUE1, property.get());
+
+    property = environment.getOptionalProperty(KEY2);
+    t.ok(property.isPresent());
+    t.equal(VALUE2, property.get());
+
+    property = environment.getOptionalProperty(KEY3);
+    t.notOk(property.isPresent());
+
+    property = environment.getOptionalProperty(KEY4);
+    t.notOk(property.isPresent());
+
+    t.end();
+  });
+
+  tap.test('getProperty', function(t) {
+    loadProperties();
+
+    t.equal(VALUE1, environment.getProperty(KEY1));
+    t.equal(VALUE2, environment.getProperty(KEY2));
+    t.notOk(environment.getProperty(KEY3));
+    t.notOk(environment.getProperty(KEY4));
+
+    t.end();
+  });
+
+  tap.test('getPropertyOr', function(t) {
+    loadProperties();
+
+    t.equal(VALUE1, environment.getPropertyOr(KEY1, DEFAULT_VALUE));
+    t.equal(VALUE2, environment.getPropertyOr(KEY2, DEFAULT_VALUE));
+    t.equal(DEFAULT_VALUE, environment.getPropertyOr(KEY3, DEFAULT_VALUE));
+    t.equal(DEFAULT_VALUE, environment.getPropertyOr(KEY4, DEFAULT_VALUE));
+
+    t.end();
+  });
+
+  tap.test('getOptionalNumber', function(t) {
+    loadNumberProperties();
+
+    var number = environment.getOptionalNumber(KEY1);
+    t.ok(number.isPresent());
+    t.equal(1000000, number.get());
+
+    number = environment.getOptionalNumber(KEY2);
+    t.ok(number.isPresent());
+    t.equal(2.5, number.get());
+
+    number = environment.getOptionalNumber(KEY3);
+    t.ok(number.isPresent());
+    t.equal(-3, number.get());
+
+    number = environment.getOptionalNumber(KEY4);
+    t.notOk(number.isPresent());
+
+    number = environment.getOptionalNumber(KEY5);
+    t.notOk(number.isPresent());
+
+    number = environment.getOptionalNumber(KEY6);
+    t.notOk(number.isPresent());
+
+    t.end();
+  });
+
+  tap.test('getNumber', function(t) {
+    loadNumberProperties();
+
+    t.equal(1000000, environment.getNumber(KEY1));
+    t.equal(2.5, environment.getNumber(KEY2));
+    t.equal(-3, environment.getNumber(KEY3));
+    t.notOk(environment.getNumber(KEY4));
+    t.notOk(environment.getNumber(KEY5));
+    t.notOk(environment.getNumber(KEY6));
+
+    t.end();
+  });
+
+  tap.test('getNumberOr', function(t) {
+    loadNumberProperties();
+
+    t.equal(1000000, environment.getNumberOr(KEY1, DEFAULT_NUMBER));
+    t.equal(2.5, environment.getNumberOr(KEY2, DEFAULT_NUMBER));
+    t.equal(-3, environment.getNumberOr(KEY3, DEFAULT_NUMBER));
+    t.equal(DEFAULT_NUMBER, environment.getNumberOr(KEY4, DEFAULT_NUMBER));
+    t.equal(DEFAULT_NUMBER, environment.getNumberOr(KEY5, DEFAULT_NUMBER));
+    t.equal(DEFAULT_NUMBER, environment.getNumberOr(KEY6, DEFAULT_NUMBER));
+
+    t.end();
+  });
+
+  tap.test('getKeys', function(t) {
+    loadProperties();
+
+    t.ok(environment.getKeys());
+
+    t.end();
+  });
+
+});
